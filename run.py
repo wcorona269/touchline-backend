ad6da6a84a14dfe19c6cc4a309b1e1b6018d7519
wcorona269 <wcorona269@gmail.com>
2023-12-04 22:17:56 -0500

    change to port 5000

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from waitress import serve
 
 # production run command - Use waitress as the production server
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8000)
+    serve(app, host='0.0.0.0', port=5000)
