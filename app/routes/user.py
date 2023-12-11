d8ba2ef3e69b8e97be0e7f653fcf54cffe3b7dd8
wcorona269 <wcorona269@gmail.com>
2023-12-11 14:41:05 -0500

    add timestamping to user avatar url

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from ..models import db, User
 from azure.storage.blob import BlobServiceClient
+from datetime import datetime
 import os
 
 bp = Blueprint('users', __name__, url_prefix='/users')
@@ -12,19 +13,20 @@ def update_avatar(username):
     try:
         file = request.files['file']
         # Upload the file to Azure Storage Blob
-        blob_name = f"avatars/{username}/{file.filename}"  # Customize the blob name as needed
+        blob_name = f"avatars/{username}/avatar"  # Customize the blob name as needed
         blob_client = container_client.get_blob_client(blob_name)
         blob_client.upload_blob(file.stream, overwrite=True)
-
         user = User.query.filter_by(username=username).first()
-        user.avatar_url = blob_client.url  # Update the 'avatar_url' field with the blob URL
-        print(blob_client.url)
-        db.session.commit()
-        result, user_info = User.get_user_info(username)
-        if result == True:
-           return jsonify({
+        if user:
+            timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+            timestamped_url = f"{blob_client.url}?t={timestamp}"
+            user.avatar_url = timestamped_url  # Update the 'avatar_url' field with the blob URL
+            db.session.commit()
+            message, user_info = User.get_user_info(username)
+            return jsonify({
                 'message': 'User info fetched successfully',
-                'user': user_info
+                'user': user.to_dict(),
+                'user_info': user_info
             }), 200
         else:
             return jsonify({
@@ -32,7 +34,7 @@ def update_avatar(username):
             }), 404
     except Exception as e:
         print(str(e))
-        return jsonify({'error': 'Failed to upload file'}), 500        
+        return jsonify({'error': 'Failed to upload file'}), 500
 
 @bp.route('/info/<username>', methods=['GET'])
 def get_user_info(username):
